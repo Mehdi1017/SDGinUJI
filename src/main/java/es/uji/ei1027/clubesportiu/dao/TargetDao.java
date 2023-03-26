b9d522c10d6086d86dc169cb5c42018daecf3832
vik0t0r <victordpa7@gmail.com>
2023-03-26 14:16:28 +0200

    dao and row mapper for target

---
+++ src/main/java/es/uji/ei1027/clubesportiu/dao/TargetDao.java
@@ -0,0 +1,87 @@
+package es.uji.ei1027.clubesportiu.dao;
+
+import es.uji.ei1027.clubesportiu.model.Target;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TargetDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public Target getTarget(String nameOds, String nameTarg) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * from subscription WHERE name_ods=? AND name_targ=?",
+                    new TargetRowMapper(),
+                    nameOds,
+                    nameTarg);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public List<Target> getAllTarget() {
+        try {
+            return jdbcTemplate.query(
+                    "SELECT * FROM Target",
+                    new TargetRowMapper());
+        }
+        catch(EmptyResultDataAccessException e) {
+            return new ArrayList<Target>();
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public void addTarget(Target target) {
+        jdbcTemplate.update(
+                "INSERT INTO subscription VALUES(?, ?, ?)",
+                target.getNameOds(),
+                target.getNameTarg(),
+                target.getDescription());
+    }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------
+    //TODO delete
+    public void deleteTarget(Target target) {
+        jdbcTemplate.update("DELETE FROM Subscription WHERE name_ods=? AND name_targ=? ",
+                target.getNameOds(), target.getNameTarg());
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // TODO update
+    public void updateTarget(Target target) {
+        jdbcTemplate.update("UPDATE Target " +
+                        "SET    description = ? ," +
+                        "WHERE  name_ods = ?"      +
+                        "WHERE  name_targ = ?",
+                target.getDescription(),
+                target.getNameOds(),
+                target.getNameTarg());
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+}
