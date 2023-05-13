9b6b0bc1ba3f6828dd4f49a045ba0012cb3ff986
vik0t0r <victordpa7@gmail.com>
2023-05-13 12:26:59 +0200

    navbar, logininfo, header partial

---
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/HomeController.java
@@ -0,0 +1,15 @@
+package es.uji.ei1027.clubesportiu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(Model model) {
+
+        model.addAttribute("CONTENT_TITLE","EI1027 - Spring MVC");
+        return "index";
+    }
+}
