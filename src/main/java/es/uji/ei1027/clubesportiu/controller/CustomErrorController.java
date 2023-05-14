6b0560276f45f6ba45f79b185d25092303491420
vik0t0r <victordpa7@gmail.com>
2023-05-14 14:34:02 +0200

    añadido pagina error bien

---
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/CustomErrorController.java
@@ -0,0 +1,46 @@
+package es.uji.ei1027.clubesportiu.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+
+
+    @RequestMapping("/error")
+    public String handleError(Model model, HttpServletRequest request) {
+        // Retrieve error details from the request and add them to the model if needed
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+
+        // Add custom attributes to the model
+        model.addAttribute("CONTENT_TITLE","Error " + statusCode);
+
+
+        // Add statusCode and exception attributes to the model
+
+        // Return the name of your modified error.html template
+        return "error";
+    }
+}
