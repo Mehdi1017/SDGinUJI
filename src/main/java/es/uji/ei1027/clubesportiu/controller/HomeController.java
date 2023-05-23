ebddba27177996e6f318461dec981b6db5df8361
vik0t0r <victordpa7@gmail.com>
2023-05-23 20:34:57 +0200

    links de los sdg funcionan

--- src/main/java/es/uji/ei1027/clubesportiu/controller/HomeController.java
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/HomeController.java
@@ -1,10 +1,15 @@
 package es.uji.ei1027.clubesportiu.controller;
 
 import es.uji.ei1027.clubesportiu.dao.ods.OdsDao;
+import es.uji.ei1027.clubesportiu.model.Ods;
+import es.uji.ei1027.clubesportiu.model.UserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -14,10 +19,108 @@ public class HomeController {
         this.odsDao = odsDao;
     }
     @GetMapping("/")
-    public String index(Model model) {
+    public String listSdg(Model model, HttpSession session) {
+
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
         model.addAttribute("CONTENT_TITLE","Viendo SDGs");
         model.addAttribute("allOds", odsDao.getAllOds());
         model.addAttribute("SELECTED_NAVBAR","SDGs");
-        return "index";
+        if (usuario == null || !usuario.isAdmin()) {
+            return "sdg/list_public";
+        }
+        else {
+            return "sdg/list_staff";
+        }
     }
+
+    @RequestMapping(value="/update/{nOds}", method = RequestMethod.GET)  // DEFINE MAPPIGN WITH PATH VARIABLE
+    public String editOds(Model model, HttpSession session,
+                          @PathVariable String nOds) {
+        model.addAttribute("CONTENT_TITLE","Editando SDG");
+        model.addAttribute("SELECTED_NAVBAR","SDGs");
+
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null || !usuario.isAdmin()) {
+            return "sdg/list_public";
+        }
+
+        model.addAttribute("ods", odsDao.getOds(nOds));  // SET MODEL ATTRIBUTE
+        return "sdg/update_staff";    // REDIRECT TO NEW VIEW WITH SET VALUES
+    }
+
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(
+            @ModelAttribute("ods") Ods ods, HttpSession session, // RETRIEVE MODEL ATTRIBUTE
+            BindingResult bindingResult) {
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null || !usuario.isAdmin()) {
+            return "redirect:/";
+        }
+
+        if (bindingResult.hasErrors())
+            return "sdg/update_staff";    // TRY AGAIN, HAD ERRORS
+        System.out.println(ods);
+        odsDao.updateOds(ods);  // UPDATE
+        String prevUrl = (String) session.getAttribute("prevURL");
+        if (prevUrl != null){
+            return "redirect:"+prevUrl;
+        }
+        return "redirect:/";     // REDIRECT SO MODEL ATTRIBUTES ARE RESTARTED
+    }
+
+    @RequestMapping(value="/view/{nOds}", method = RequestMethod.GET)  // DEFINE MAPPIGN WITH PATH VARIABLE
+    public String viewOds(Model model, HttpSession session,
+                          @PathVariable String nOds) {
+
+        Ods ods = odsDao.getOds(nOds);
+        model.addAttribute("CONTENT_TITLE","Visualizando SDG: "+ods.getNameOds());
+        model.addAttribute("SELECTED_NAVBAR","SDGs");
+        model.addAttribute("ods", ods);  // SET MODEL ATTRIBUTE
+
+        session.setAttribute("prevURL","/view/"+nOds);
+
+
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null || !usuario.isAdmin()) {
+            return "sdg/view_public";
+        }
+
+        return "sdg/view_staff";
+    }
+
+    // TODO
+    @RequestMapping(value="/add}", method = RequestMethod.GET)
+    public String addOds(Model model, HttpSession session) {
+        model.addAttribute("CONTENT_TITLE","Editando SDG");
+        model.addAttribute("SELECTED_NAVBAR","SDGs");
+
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null || !usuario.isAdmin()) {
+            return "sdg/list_public";
+        }
+
+        model.addAttribute("ods", odsDao.getOds(nOds));
+        return "sdg/update_staff";
+    }
+
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public String processAddSubmit(
+            @ModelAttribute("ods") Ods ods, HttpSession session,
+            BindingResult bindingResult) {
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null || !usuario.isAdmin()) {
+            return "redirect:/";
+        }
+
+        if (bindingResult.hasErrors())
+            return "sdg/update_staff";
+        System.out.println(ods);
+        odsDao.updateOds(ods);
+        String prevUrl = (String) session.getAttribute("prevURL");
+        if (prevUrl != null){
+            return "redirect:"+prevUrl;
+        }
+        return "redirect:/";
+    }
+
 }
