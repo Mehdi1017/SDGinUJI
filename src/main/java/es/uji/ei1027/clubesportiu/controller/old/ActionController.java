b3bc22c8cab484fd3a942077edadf4343e57393a
mehdi <mehdigassabarsa@gmail.com>
2023-05-28 11:03:44 +0200

    listando acciones

--- src/main/java/es/uji/ei1027/clubesportiu/controller/old/ActionController.java
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/old/ActionController.java
@@ -54,37 +54,5 @@ public class ActionController {
 
 //    // -----------------------------------------------------------------------------------------------------------------
 //    // -----------------------------------------------------------------------------------------------------------------
-//
-//    @RequestMapping(value="/update/{nNadador}/{nProva}", method = RequestMethod.GET)  // DEFINE MAPPIGN WITH PATH VARIABLE
-//    public String editClassificacio(Model model,
-//                                    @PathVariable String nNadador,
-//                                    @PathVariable String nProva) {  // RETRIEVE PATH VARIABLE
-//        model.addAttribute("classificacio", classificacioDao.getClassificacio(nNadador, nProva));  // SET MODEL ATTRIBUTE
-//        return "classificacio/update";    // REDIRECT TO NEW VIEW WITH SET VALUES
-//    }
-//
-//    @RequestMapping(value="/update", method = RequestMethod.POST)
-//    public String processUpdateSubmit(
-//            @ModelAttribute("classificacio") Classificacio classificacio, // RETRIEVE MODEL ATTRIBUTE
-//            BindingResult bindingResult) {
-//        if (bindingResult.hasErrors())
-//            return "classificacio/update";    // TRY AGAIN, HAD ERRORS
-//        System.out.println(classificacio);
-//        classificacioDao.updateClassificacio(classificacio);  // UPDATE
-//        return "redirect:list";     // REDIRECT SO MODEL ATTRIBUTES ARE RESTARTED
-//    }
-//
-//    // -----------------------------------------------------------------------------------------------------------------
-//    // -----------------------------------------------------------------------------------------------------------------
-//
-//    @RequestMapping(value = "/delete/{nNadador}/{nProva}")
-//    public String processDeleteClassif(@PathVariable String nNadador,
-//                                       @PathVariable String nProva) {
-//        classificacioDao.deleteClassificacio(nNadador, nProva);
-//        return "redirect:../../list";
-//    }
-//
-//    // -----------------------------------------------------------------------------------------------------------------
-//    // -----------------------------------------------------------------------------------------------------------------
 
 }
