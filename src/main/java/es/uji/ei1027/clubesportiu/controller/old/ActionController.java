1a5996b4cbe32fb78c9f196f83a162e83114e9fe
mehdi <mehdigassabarsa@gmail.com>
2023-05-28 12:51:55 +0200

    se añaden las acciones

--- src/main/java/es/uji/ei1027/clubesportiu/controller/old/ActionController.java
+++ src/main/java/es/uji/ei1027/clubesportiu/controller/old/ActionController.java
@@ -1,17 +1,19 @@
 package es.uji.ei1027.clubesportiu.controller.old;
 
 
+import es.uji.ei1027.clubesportiu.controller.Initiative.ActionValidator;
+import es.uji.ei1027.clubesportiu.controller.Initiative.InitiativeValidator;
 import es.uji.ei1027.clubesportiu.dao.action.ActionDao;
+import es.uji.ei1027.clubesportiu.dao.initiative.InitiativeDao;
+import es.uji.ei1027.clubesportiu.dao.target.TargetDao;
 import es.uji.ei1027.clubesportiu.model.Action;
+import es.uji.ei1027.clubesportiu.model.Initiative;
 import es.uji.ei1027.clubesportiu.model.UserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -23,10 +25,20 @@ public class ActionController {
     // -----------------------------------------------------------------------------------------------------------------
 
     private ActionDao actionDao;
+    private TargetDao targetDao;
+    private InitiativeDao initiativeDao;
 
     @Autowired
     public void setActionDao(ActionDao actionDao) {
         this.actionDao = actionDao;
+    }
+    @Autowired
+    public void setInitiativeDao(InitiativeDao initiativeDao) {
+        this.initiativeDao = initiativeDao;
+    }
+    @Autowired
+    public void setActionDao(TargetDao targetDao) {
+        this.targetDao = targetDao;
     }
 
     // -----------------------------------------------------------------------------------------------------------------
@@ -36,13 +48,14 @@ public class ActionController {
     public String listAction(Model model, HttpSession session, @PathVariable String nIni) {
         UserDetails usuario = (UserDetails) session.getAttribute("user");
         if (usuario == null){
-            session.setAttribute("nextUrl", "/myInitiative/list");
+            session.setAttribute("nextUrl", "/action/list");
             return "redirect:/login";
         }
 
         model.addAttribute("CONTENT_TITLE","Mostrando Acciones 📋");
         model.addAttribute("SELECTED_NAVBAR","Área privada");
         model.addAttribute("actions", actionDao.getActions(nIni));
+        session.setAttribute("nIni", nIni);
         return "action/list";
     }
 
@@ -50,18 +63,68 @@ public class ActionController {
     // -----------------------------------------------------------------------------------------------------------------
 
     @RequestMapping(value="/add")
-    public String addTarget(Model model) {
-        model.addAttribute("action", new Action());  // SET MODEL ATTRIBUTE
-        return "/old/action/add";
+    public String addAction(Model model, HttpSession session) {
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null){
+            session.setAttribute("nextUrl", "/myInitiative/add");
+            return "redirect:/login";
+        }
+
+        String nIni = (String) session.getAttribute("nIni");
+        model.addAttribute("CONTENT_TITLE","Creando una Accion 📝"); // page data
+        model.addAttribute("SELECTED_NAVBAR","Área privada");
+
+        Initiative initiative = initiativeDao.getInitiative(nIni);
+        session.setAttribute("tmp_initiative", initiative);
+
+        model.addAttribute("targList", targetDao.getTargetByOds(initiative.getNameOds()));  // needed data
+
+        model.addAttribute("action", new Action());
+
+//        System.out.println("Ejecuta add controller");
+
+        return "action/add";
     }
 
-    @RequestMapping(value="/add", method= RequestMethod.POST)
-    public String processAddSubmit(@ModelAttribute("action") Action action,  // RETRIEVE MODEL ATTRIBUTE
-                                   BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
+
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public String processAddSubmit(
+            @ModelAttribute("action") Action action,
+            @SessionAttribute("tmp_initiative") Initiative initiative,
+            BindingResult bindingResult, Model model, HttpSession session) {
+
+        UserDetails usuario = (UserDetails) session.getAttribute("user");
+        if (usuario == null){
+            session.setAttribute("nextUrl", "/action/add");
+            return "redirect:/login";
+        }
+
+        model.addAttribute("SELECTED_NAVBAR","Área privada");
+
+        // validate new action
+        ActionValidator actionValidator = new ActionValidator(initiative);
+        actionValidator.validate(action, bindingResult);
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("CONTENT_TITLE","Creando una Accion 📝");
+            model.addAttribute("SELECTED_NAVBAR","Área privada");
+            model.addAttribute("targList", targetDao.getTargetByOds(initiative.getNameOds()));  // needed data
+            session.setAttribute("tmp_initiative", initiative);
             return "action/add";
+        }
+
+        // complete & add action to persistent initiative
+        action.setNameInitiative(initiative.getNameIni());
+        action.setNameOds(initiative.getNameOds());
         actionDao.addActionn(action);
-        return "redirect:list";
+
+        model.addAttribute("CONTENT_TITLE","Mostrando Acciones 📋");
+        model.addAttribute("SELECTED_NAVBAR","Área privada");
+        String nIni = initiative.getNameIni();
+        model.addAttribute("actions", actionDao.getActions(nIni));
+        session.setAttribute("nIni", nIni);
+
+        return "action/list";
     }
 
 //    // -----------------------------------------------------------------------------------------------------------------
